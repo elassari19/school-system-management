09eb47ab650fb25560c25420c20cd12b8bbdc83a
elassari19 <elassari19@gmail.com>
2024-09-23 21:43:49 +0100

    add jest test configurations

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
+import { creatApp } from './app';
 import swaggerDocs from './utils/swagger';
-/* ROUTE IMPORTS */
-import { getUser, getUsers } from './routes/user.route';
 
-/* CONFIGURATIONS */
-dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-/* ROUTES */
-app.use('/v1/api', getUser);
-app.use('/v1/api', getUsers);
+const app = creatApp();
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3001;
